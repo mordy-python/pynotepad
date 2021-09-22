03298be75d35d5e104ac7e5b8a53e49eb78f08be
mordy <imky171@gmail.com>
2021-09-21 20:53:52 -0400

    menued

--- notepad.py
+++ notepad.py
@@ -1,15 +1,10 @@
 import PySimpleGUI as psg
 
-menu_def = [['&File', ['&Open     Ctrl-O', '&Save       Ctrl-S', '&Properties', 'E&xit']],
-                ['&Edit', ['&Paste', ['Special', 'Normal', ], 'Undo'], ],
-                ['&Toolbar', ['---', 'Command &1', 'Command &2',
-                              '---', 'Command &3', 'Command &4']],
-                ['&Help', '&About...'], ]
-
-app_layout = [
-    [psg.Menu(menu_def)],
-    [psg.Multiline()]
+menu_def = [
+    ["&File", ["&Open     Ctrl-O", "&Save       Ctrl-S", "E&xit"]],
 ]
+
+app_layout = [[psg.Menu(menu_def)], [psg.Multiline()]]
 
 window = psg.Window("PyNotepad", layout=app_layout, resizable=True)
 
