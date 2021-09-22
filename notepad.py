158dd51f3a7dc67be9ba1bf807ef73e3a60e63d6
Mechel <mechel.waldner@gmail.com>
2021-09-21 20:27:27 -0500

    made changes

--- notepad.py
+++ notepad.py
@@ -1,14 +1,13 @@
 import PySimpleGUI as psg
 
-while True:
-    event,values=window.read()
-
-    if event == None:
-        break
-
-window.close
 app_layout = [
     [psg.Menu(['File', 'Help'])]
 ]
 
 window = psg.Window('PyNotepad', layout=app_layout)
+while True:
+    event,values=window.read()
+
+    if event == None:
+        break
+window.close()
